a2535da5a0fe75cd03b44fc12d0d3b39fe514a5c
MateuszKlepacki1 <mateusz.klepacki@gmail.com>
2022-08-05 12:32:38 +0200

    Basic calculator

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+    /*/
+    1. dodawanie
+    2. odejmownie
+    3. mnożenie
+    4. dzielenie
+     */
+    public int summary(int a, int b){
+        return a+b;
+    }
+    public int subtraction (int a, int b) {
+        return a-b;
+    }
+    public int multiplication (int a, int b){
+        return a*b;
+    }
+    public int division (int a, int b){
+        return a/b;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 /*
         Car mini = new Car("White", 3,"Cooper S",true);
@@ -7,7 +9,7 @@ public class Main {
     }
 
 }
- */
+
         System.out.println("Choose color of your car");
         Scanner Colour = new Scanner(System.in);
         String newColour = Colour.next();
@@ -23,5 +25,41 @@ public class Main {
 
         Car Mini=  new Car(newColour, newDoorsAmount,newBrand,newAutomaticGearbox);
         System.out.println("So Your car will be: "+ Mini.getBrand()+ " . Color of your car is: " + Mini.getColour() + ". Your "+ Mini.getBrand()+ " will have " + Mini.getDoorsAmount() + " doors. Also, You will have an gerabox (True - you have an automatic gearbox / False - youhave manual gearbox): "+ Mini.getAutomaticGearbox() + ".");
+    }
+}
+*/
+                /*/
+        int a;
+        int b;
+        a=10;
+        b=2;
+        int c;
+        c=a-b;
+        System.out.println(c);
+
+
+        Calculator calc = new Calculator();
+        calc.summary(5, 2);
+        System.out.println("The result of summary is: " + calc.summary(5, 2));
+
+        calc.subtraction(9, 4);
+        System.out.println("The result of subtraction is: " + calc.subtraction(9, 4));
+
+        calc.multiplication(4, 3);
+        System.out.println("The result of multiplication is: " + calc.multiplication(4, 3));
+
+        calc.division(8, 4);
+        System.out.println("The result of division is: " + calc.division(8, 4));
+*/
+        Calculator calc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give first number: ");
+        int firstVariable = scanner.nextInt();
+        System.out.println("Give second number: ");
+        int secondVariable = scanner.nextInt();
+        System.out.println("The summary result is: " + calc.summary(firstVariable, secondVariable));
+        System.out.println("The subtraction result is: " + calc.subtraction(firstVariable,secondVariable));
+        System.out.println("The multiplication result is: " + calc.multiplication(firstVariable,secondVariable));
+        System.out.println("The division result is: " + calc.division(firstVariable,secondVariable));
     }
 }
