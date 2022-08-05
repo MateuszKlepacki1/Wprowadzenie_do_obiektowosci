5c08db75d23100d8c26dacf9300186ffc088c2f6
MateuszKlepacki1 <mateusz.klepacki@gmail.com>
2022-08-05 14:03:57 +0200

    User can choose what he want to do in bsaic calculator ('double' function to division)

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
     public int multiplication (int a, int b){
         return a*b;
     }
-    public int division (int a, int b){
+    public double division (double a, double b){
         return a/b;
     }
 
